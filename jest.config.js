0eb64c014f27b786742168b305ad6319b1203529
sarinazeitooni <zeitoonisarina@gmail.com>
2021-07-06 18:11:53 +0430

    added tests

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,35 @@
 module.exports = {
-    collectCoverageFrom: [
-        '**/*.{js,jsx,ts,tsx}',
-        '!**/*.d.ts',
-        '!**/node_modules/**',
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js",
+        "jsx"
     ],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
     transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+        '^.+\\.jsx?$': 'babel-jest',
     },
-    moduleDirectories: ['node_modules', '<rootDir>/src'],
-
-    transformIgnorePatterns: [
-        '/node_modules/',
-        '^.+\\.module\\.(css|sass|scss)$',
+    testMatch: [
+        "**/*.(test|spec).(js|jsx)"
+    ],
+    globals: {
+        "ts-jest": {
+            useBabelrc: true,
+            tsConfigFile: "jest.tsconfig.json"
+        }
+    },
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "enzyme.js"
+    ],
+    setupTestFrameworkScriptFile: "<rootDir>/enzyme.js",
+    coverageReporters: [
+        "json",
+        "lcov",
+        "text",
+        "text-summary"
     ],
     moduleNameMapper: {
         '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
         '(webp|jpeg)$': 'identity-obj-proxy',
     },
-    "collectCoverage": true,
-    "coverageReporters": ["lcov"],
-    "coverageDirectory": "test-coverage",
-    "coverageThreshold": {
-        "global": {
-            "branches": 0,
-            "functions": 0,
-            "lines": 0,
-            "statements": 0
-        }
-    },
-}
+};
